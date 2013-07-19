5724810596c2252f2655e98bcee471a325634c44
havenwood <shannonskipper@gmail.com>
2013-07-19 11:25:35 -0700

    Serialize with Marshal instead of byte-slicing.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -8,17 +8,17 @@ module Encrypt
       iv = cipher.random_iv
       salt = OpenSSL::Random.random_bytes(16)
       cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, 20000, 32)
-      salt << iv << cipher.update(self) << cipher.final
+      Marshal.dump([salt, iv, cipher.update(self) << cipher.final])
     end
 
     def decrypt key
       if self.bytesize > 32
+        salt, iv, encrypted_data = Marshal.load(self)
         cipher = OpenSSL::Cipher::AES256.new(:CTR)
         cipher.decrypt
-        salt = self.byteslice(0..15)
         cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(key, salt, 20000, 32)
-        cipher.iv = self.byteslice(16..31)
-        cipher.update(self.byteslice(32..-1)) << cipher.final
+        cipher.iv = iv
+        cipher.update(encrypted_data) << cipher.final
       else
         nil
       end
